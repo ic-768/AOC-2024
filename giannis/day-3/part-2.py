35e0f9375ea2c45acdd8fe3d606ce428d96f257e
ic768 <ic768@protonmail.com>
2024-12-04 11:28:07 +0200

    Day 3 done

--- giannis/day-3/part-2.py
+++ giannis/day-3/part-2.py
@@ -4,14 +4,20 @@ import re
 mult_pattern = r"mul\(\d+,\d+\)"
 # matches the numbers
 operand_pattern = r"\d+,\d+"
+# matches don't block
+dont_pattern = r"(?<=don't).*?(?=\bdon't\b|do)"
+
 
 with open("input.txt", "r") as file:
     text = file.read()
+    # clean don't blocks
+    cleaned_text = re.sub(dont_pattern, "", text, flags=re.DOTALL)
 
+    # same as part 1
     sum = 0
-    results = re.findall(mult_pattern, text)
+    results = re.findall(mult_pattern, cleaned_text)
     for multiplication in results:
         operand_results = re.findall(operand_pattern, multiplication)
         operands = [int(operand) for operand in operand_results[0].split(",")]
         sum += operands[0] * operands[1]
-print(sum)
+    print(sum)
