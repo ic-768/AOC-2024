97185e6e49abc0ec3549ea597304407c76d3ebca
ic768 <ic768@protonmail.com>
2024-12-05 18:39:00 +0200

    Day 5 part 2

--- giannis/day-5/part-1.py
+++ giannis/day-5/part-1.py
@@ -3,24 +3,29 @@ import re
 sum = 0
 
 with open("input.txt", "r") as file:
-    (r, p) = file.read().split("\n\n")
+    (rule_section, page_section) = file.read().split("\n\n")
 
-    rules = r.split("\n")
-    page_lines = p.split("\n")
+    rules = rule_section.split("\n")
+    rules = [re.findall(r"(\d+)", item) for item in rules]
+    rules = [[int(num) for num in pair] for pair in rules]
+
+    page_lines = page_section.split("\n")
     page_lines = [item.split(",") for item in page_lines]
     page_lines.pop()
     page_lines = [[int(num) for num in item] for item in page_lines]
 
-    rules = [re.findall(r"(\d+)", item) for item in rules]
-    rules = [[int(num) for num in pair] for pair in rules]
-
     for line in page_lines:
         for index, rule in enumerate(rules):
+            # rule applies
             if rule[0] in line and rule[1] in line:
+                # rule is broken
                 if line.index(rule[1]) < line.index(rule[0]):
                     break
 
-            if index == len(rules) - 1:
-                sum += line[len(line) // 2]
+            # passed all rules successfully
+            is_last_rule = index == len(rules) - 1
+            if is_last_rule:
+                middle_element = line[len(line) // 2]
+                sum += middle_element
 
 print(sum)
---
+++ giannis/day-5/part-2.py
@@ -0,0 +1,48 @@
+import re
+
+sum = 0
+bad_lines = []
+
+with open("input.txt", "r") as file:
+    (rule_section, page_section) = file.read().split("\n\n")
+
+    rules = rule_section.split("\n")
+    rules = [re.findall(r"(\d+)", item) for item in rules]
+    rules = [[int(num) for num in pair] for pair in rules]
+
+    page_lines = page_section.split("\n")
+    page_lines = [item.split(",") for item in page_lines]
+    page_lines.pop()
+    page_lines = [[int(num) for num in item] for item in page_lines]
+
+    for line in page_lines:
+        for index, rule in enumerate(rules):
+            if rule[0] in line and rule[1] in line:
+                if line.index(rule[1]) < line.index(rule[0]):
+                    bad_lines.append(line)
+                    break
+
+    for line in bad_lines:
+        has_error = True
+        while has_error:
+            for index, rule in enumerate(rules):
+                # rule applies
+                if rule[0] in line and rule[1] in line:
+                    index1 = line.index(rule[0])
+                    index2 = line.index(rule[1])
+
+                    # rule is broken
+                    if index2 < index1:
+                        # swap
+                        line[index1], line[index2] = line[index2], line[index1]
+                        break
+
+                # passed all rules successfully
+                is_last_rule = index == len(rules) - 1
+                if is_last_rule:
+                    middle_element = line[len(line) // 2]
+                    sum += middle_element
+                    has_error = False
+
+
+print(sum)
