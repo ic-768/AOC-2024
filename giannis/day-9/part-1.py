950974e7fd194aea6825c3e051da6d7c00149831
ic768 <ic768@protonmail.com>
2024-12-09 15:40:49 +0200

    Day 9 part 1 done

--- giannis/day-9/part-1.py
+++ giannis/day-9/part-1.py
@@ -5,24 +5,27 @@ with open("input.txt", "r") as file:
     char_number = 0
 
     decoded = []
-    print("before first for loop")
     for x, char in enumerate(text):
         not_free = x % 2 == 0
 
         if not_free:
-            decoded.extend(str(char_number) * int(char))
+            # we don't use .extend because it would combine multi-digit numbers into one entry
+            for i in range(int(char)):
+                # append them as many times as needed
+                decoded.append(str(char_number))
             char_number += 1
 
         else:
             decoded.extend("." * int(char))
 
-
-print("before second for loop")
 for i in range(len(decoded) - 1, -1, -1):
     first_free = decoded.index(".")
+
+    # done sorting
     if first_free > i:
         break
 
+    # swap number for free space
     if re.match(r"\d", decoded[i]):
         decoded[first_free] = decoded[i]
         decoded[i] = "."
@@ -31,7 +34,8 @@ for i in range(len(decoded) - 1, -1, -1):
 sum = 0
 filtered = [x for x in decoded if x != "."]
 
-for x, char in enumerate(filtered):
-    sum += int(char) * x
 
-print(sum)
+for i, each in enumerate(filtered):
+    sum += int(each) * i
+
+print("sum is", sum)
