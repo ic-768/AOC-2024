68645a4077c2f876a7522dc6e1814aad043308df
ic768 <ic768@protonmail.com>
2024-12-14 00:27:48 +0200

    Day 12 part 2 wip

--- giannis/day-12/part-2.py
+++ giannis/day-12/part-2.py
@@ -43,86 +43,107 @@ with open("sample.txt") as file:
                 regions.append(region_to_fill)
 
     regions_sum = 0
-    for region in regions:
+    for y, region in enumerate(regions):
         perimeter = 0
         area = len(region)
 
-        # keep track of x AND y coordinate, second one is which one was modified to find a border,
-        # and which direction it was modified towards
-        # i.e. ('y', 3, -1)
+        # check to find corners and add them instead of sides
+        # corner if : nothing top, left
+        #             nothing top, right
+        #             nothing bottom, left
+        #             nothing bottom, right
 
-        # example:          01
-        #          line 0:  EE
-        #               1:  E
-        #               2:  EE
-        # starting from top going counter clockwise
-        #
-
-        # up
-        # ('0','y',0,-1) -> next visits check to see if already counted
-        # left
-        # ('0','x',0,-1)
-        # down
-        # ('0','y',2,+1)
-        # right
-        # ('2','x',1,+1)
-        # up
-        # ('1','y',2,-1)
-        # right
-        # ('1','x',0,+1)
-        # down
-        # ('1','y',0,+1)
-        # right
-        # ('0','x',1,+1)
-
-        counted_sides = set()
+        # AAAAA
+        # BBCCC
+        # BBCCC
+        # EEECC
 
         for char in region:
-            # look on all sides and see if they belong to a different plant.
-            # else, doesn't add to perimeter
             x = char[1]
             y = char[2]
             plant = char[0]
 
-            # if changed direction is same, is other direction sequential if yes, then don't add more
-            if (y - 1, "x", x, -1) not in counted_sides or (
-                y + 1,
-                "x",
-                x,
-                +1,
-            ) not in counted_sides:
-                if x - 1 >= 0:
-                    if lines[y][x - 1] != plant:
-                        counted_sides.add((y, "x", x, -1))
-                        perimeter += 1
-                else:
-                    counted_sides.add((y, "x", x, -1))
-                    perimeter += 1
-                if x + 1 < len(lines[0]):
-                    if lines[y][x + 1] != plant:
-                        counted_sides.add((y, "x", x, +1))
-                        perimeter += 1
-                else:
-                    counted_sides.add((y, "x", x, +1))
-                    perimeter += 1
+            nothing_left = (x - 1 < 0) or (lines[y][x - 1] != plant)
+            nothing_top = (y - 1 < 0) or (lines[y - 1][x] != plant)
+            nothing_right = (x + 1 >= len(lines[0])) or (lines[y][x + 1] != plant)
+            nothing_bottom = (y + 1 >= len(lines)) or (lines[y + 1][x] != plant)
 
-            if (x - 1, "y", y, -1) or (x + 1, "y", y, +1) not in counted_sides:
-                if y - 1 >= 0:
-                    if lines[y - 1][x] != plant:
-                        counted_sides.add((x, "y", y, -1))
-                        perimeter += 1
-                else:
-                    counted_sides.add((x, "y", y, -1))
-                    perimeter += 1
-                if y + 1 < len(lines):
-                    if lines[y + 1][x] != plant:
-                        counted_sides.add((x, "y", y, +1))
-                        perimeter += 1
-                else:
-                    counted_sides.add((x, "y", y, +1))
-                    perimeter += 1
+            nothing_bottom_left = (
+                (y + 1 >= len(lines) or x - 1 < 0 or lines[y + 1][x - 1] != plant)
+                and (x - 1 < 0 or lines[y][x - 1] != plant)
+                and (y + 1 >= len(lines) or lines[y + 1][x] != plant)
+            )
 
-        print(region[0][0], "counted sides are", counted_sides)
+            nothing_bottom_right = (
+                (
+                    y + 1 >= len(lines)
+                    or x + 1 >= len(lines[0])
+                    or lines[y + 1][x + 1] != plant
+                )
+                and (x + 1 >= len(lines[0]) or lines[y][x + 1] != plant)
+                and (y + 1 >= len(lines) or lines[y + 1][x] != plant)
+            )
 
-        # regions_sum += perimeter * area
-        # print(regions_sum)
+            nothing_top_left = (
+                (y - 1 < 0 or x - 1 < 0 or lines[y - 1][x - 1] != plant)
+                and (x - 1 < 0 or lines[y][x - 1] != plant)
+                and (y - 1 < 0 or lines[y - 1][x] != plant)
+            )
+
+            nothing_top_right = (
+                # top-right is out of bounds or another plant
+                (y - 1 < 0 or x + 1 >= len(lines[0]) or lines[y - 1][x + 1] != plant)
+                and
+                # no/different plant directly to the right
+                (
+                    (x + 1 >= len(lines[0]) or lines[y][x + 1] != plant)
+                    # no/different plant directly above
+                    and (y - 1 < 0 or lines[y - 1][x] != plant)
+                )
+                # no/different plant directly to the top
+                and (y - 1 < 0 or lines[y - 1][x] != plant)
+            )
+
+            inverted_top_right = (
+                # top-right is out of bounds or another plant
+                (y - 1 < 0 or x + 1 >= len(lines[0]) or lines[y - 1][x + 1] != plant)
+                # Check for L shape ( one above and one to the right
+                and (x + 1) < len(lines[0])
+                and y - 1 >= 0
+                and lines[y][x + 1] == plant
+                and lines[y - 1][x] == plant
+            )
+
+            inverted_top_left = (
+                # top-left is out of bounds or another plant
+                (y - 1 < 0 or x - 1 < 0 or lines[y - 1][x - 1] != plant)
+                # Check for L shape ( one above and one to the left
+                and x - 1 >= 0
+                and y - 1 >= 0
+                and lines[y][x - 1] == plant
+                and lines[y - 1][x] == plant
+            )
+
+            # shouldn't have something to it's right OR also have something above it
+
+            if nothing_bottom_left:
+                # print("bottom-left")
+                perimeter += 1
+
+            if nothing_bottom_right:
+                # print("bottom-right")
+                perimeter += 1
+
+            if nothing_top_left:
+                # print("top-left")
+                perimeter += 1
+
+            if nothing_top_right:
+                # print("top-right")
+                perimeter += 1
+
+            if inverted_top_right:
+                print(char)
+                print("inverted top-right")
+                perimeter += 1
+        regions_sum += perimeter * area
